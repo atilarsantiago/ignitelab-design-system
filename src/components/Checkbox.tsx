1b3f6712e29db2b6c31a7ad8d8267be22878a001
Atila R. Santiago <atilarsantiago@gmail.com>
2022-11-01 11:15:18 -0300

    update checkbox

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -2,6 +2,7 @@ import * as CheckboxPrimitive from '@radix-ui/react-checkbox';
 import {Check} from 'phosphor-react'
 
 
+
 export interface CheckboxProps{}
 
 export function Checkbox({}:CheckboxProps){
